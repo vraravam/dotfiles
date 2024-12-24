f06b0c7781e4b5400381cd55f8debc105cee7936
jothee <jotheeswaran-dev@noreply.users.github.com>
2024-12-24 23:00:35 +0530

    Add. verify touchID hardware present before approve-fingerprint-sudo script

--- scripts/approve-fingerprint-sudo.sh
+++ scripts/approve-fingerprint-sudo.sh
@@ -5,9 +5,18 @@
 # To be able to use the mac touchbar for authorizing the 'sudo' command in terminal
 # This will persist through software updates unlike changes directly made to '/etc/pam.d/sudo'
 # Copied from: https://apple.stackexchange.com/a/466029
-# TODO: Need to ensure that TouchId hardware is present before running this script
 
 type is_file &> /dev/null 2>&1 || source "${HOME}/.shellrc"
+
+function check_touchid_hardware() {
+  ioreg -c AppleBiometricSensor | grep -q "AppleBiometricSensor"
+  return $? # Exit status 0 if found, non-zero otherwise
+}
+
+if ! check_touchid_hardware; then
+  echo "Touch ID hardware is not detected. Exiting script."
+  exit 1
+fi
 
 if ! is_file /etc/pam.d/sudo_local; then
   sudo sh -c 'sed "s/^#auth/auth/" /etc/pam.d/sudo_local.template > /etc/pam.d/sudo_local'
