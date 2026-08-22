4dff3715748ba55af66068e7d31fd64e5469729d
Vijay A <vraravam@users.noreply.github.com>
2026-08-22 15:28:43 +0530

    [WIP] convert fresh-install-of-osx.sh to ruby
    TODO before merging to master:
    - Testing on vanilla OS and pre-configured machine
    - Update all documentation references from .sh to .rb file extension
      (currently kept as .sh in comments and docs for compatibility)

--- scripts/utilities/keybase.rb
+++ scripts/utilities/keybase.rb
@@ -45,6 +45,14 @@ module Keybase
     EnvVars::KEYBASE_USERNAME
   end
 
+  # Builds the keybase:// URL for the given repo name owned by KEYBASE_USERNAME.
+  #
+  # @param repo_name [String]
+  # @return [String] keybase://private/username/repo_name
+  def build_repo_url(repo_name)
+    "keybase://private/#{username}/#{repo_name}"
+  end
+
   # Returns true if the URL is a Keybase git repo URL (keybase://...).
   #
   # @param url [String]
